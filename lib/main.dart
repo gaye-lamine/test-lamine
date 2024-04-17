a32d855f26457b8eef9dc06e6832e41c7f63bd5f
abda-gaye <lamineg049@gmail.com>
2024-04-17 13:04:06 +0000

    integration liste gare et liste trains

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:terappmobile/screens/auth/cgu.dart';
-import 'package:terappmobile/screens/auth/deliverytrack.dart';
-import 'package:terappmobile/screens/home/abonnement.dart';
-import 'package:terappmobile/screens/home/accueil.dart';
-import 'package:terappmobile/screens/home/carte_abonnement.dart';
-import 'package:terappmobile/screens/home/home.dart';
-import 'package:terappmobile/screens/home/modify_profile.dart';
-import 'package:terappmobile/screens/home/profile.dart';
-import 'package:terappmobile/screens/auth/info_perso.dart';
-import 'package:terappmobile/screens/auth/otp.dart';
-import 'package:terappmobile/screens/auth/sign_up.dart';
 import 'package:terappmobile/screens/auth/splash.dart';
-import 'package:terappmobile/screens/train/suivi_voyage.dart';
-import 'package:terappmobile/screens/train/train_voyage.dart';
-import 'package:terappmobile/screens/auth/welcome.dart';
-import 'package:terappmobile/utils/app_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,11 +12,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       //title: 'Flutter Demo',
-       theme: ThemeData(
-       // colorScheme: AppColors.marron,
+      theme: ThemeData(
+        // colorScheme: AppColors.marron,
         useMaterial3: true,
       ),
-      home: Accueil(),
+      home: const SplashScreen(),
     );
   }
 }
